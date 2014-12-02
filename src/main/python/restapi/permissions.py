d9fff0a62ccc25e0378fc317117f8f19485489a4
amitmohleji <amit.mohleji@gmail.com>
2014-12-01 21:27:03 -0500

    changes to permissions.py for pulling roles for the logged in user

--- src/main/python/restapi/permissions.py
+++ src/main/python/restapi/permissions.py
@@ -3,7 +3,8 @@ import os
 def exportSecurityToJSON():
 
 	roles = dict()
-	for role in roleService.list():
+	#for role in roleService.list():
+	for role in roleService.listMyRoles():
 		permissions = dict()
 		roleMap = permissionService.getGrantedPermissions(role)
 		for key in roleMap.keys():
